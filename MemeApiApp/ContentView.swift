8867ebb42c342b7daa3c9009e608ca5e8b74eabb
Johnny <johsun240@gmail.com>
2024-04-17 10:02:51 -0500

    Initial Commit

---
+++ MemeApiApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemeApiApp
+//
+//  Created by SUN, JOHNNY on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemeApiApp/MemeApiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeApiAppApp.swift
+//  MemeApiApp
+//
+//  Created by SUN, JOHNNY on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MemeApiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
