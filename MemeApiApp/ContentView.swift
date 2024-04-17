d7f728f9f0b3a947f229d0232f7a9358d518c9f4
AidenScottStich <stichaiden2@gmail.com>
2024-04-17 10:24:45 -0500

    Get Random Meme

--- MemeApiApp/ContentView.swift
+++ MemeApiApp/ContentView.swift
@@ -3,38 +3,69 @@
 //  ApiMobile
 //
 //  Created by STICH, AIDEN S. on 4/15/24.
-// Im trying to commit
-
+//
+ 
 import SwiftUI
 import Foundation
 import URLImage
-
+ 
 struct Meme: Codable, Identifiable {
     let id: String
     let name: String
     let url: URL
 }
-
+ 
 struct MemeResponse: Codable {
     let data: MemeData
 }
-
+ 
 struct MemeData: Codable {
     let memes: [Meme]
 }
-
+ 
 struct ContentView: View {
     @State private var memes: [Meme] = []
-
+    @State private var selectedNum :Int = 0
+ 
     var body: some View {
         VStack {
+            Button(action: {
+                selectedNum = Int.random(in: 1...memes.count)
+            }, label: {
+                Text("Get Random Meme")
+            })
+            
+            if(selectedNum != 0){
+                Text(memes[selectedNum].name)
+                    .font(.title)
+//                RemoteImage(url: memes[selectedNum].url)
+//                    .aspectRatio(contentMode: .fit)
+//                    .frame(width: 200, height: 200)
+                AsyncImage(url: memes[selectedNum].url) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    case .failure:
+                        Image(systemName: "photo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    @unknown default:
+                        EmptyView()
+                    }
+                }
+            }
+            
             Button(action: {
                 fetchMemes()
                 print("Button is clicked")
             }, label: {
                 Text("Fetch Memes")
             })
-
+ 
             ScrollView {
                 LazyVStack {
                     ForEach(memes) { meme in
@@ -52,13 +83,13 @@ struct ContentView: View {
         }
         .padding()
     }
-
+ 
     func fetchMemes() {
         guard let url = URL(string: "https://api.imgflip.com/get_memes") else {
             print("Invalid URL")
             return
         }
-
+ 
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let jsonData = data {
                 do {
@@ -74,11 +105,11 @@ struct ContentView: View {
         }.resume()
     }
 }
-
+ 
 struct RemoteImage: View {
     let url: URL
     @State private var image: Image? = nil
-
+ 
     var body: some View {
         Group {
             if let image = image {
@@ -90,7 +121,7 @@ struct RemoteImage: View {
             }
         }
     }
-
+ 
     private func loadImage() {
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, error == nil else { return }
@@ -102,8 +133,8 @@ struct RemoteImage: View {
         }.resume()
     }
 }
-
-
+ 
+ 
 #Preview {
     ContentView()
 }
